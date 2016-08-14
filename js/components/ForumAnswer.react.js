899eb4f351aa977d59ead9cc7915ff9dc0b1a30d
AleksicIvan <g.zunice@gmail.com>
2016-08-14 22:17:07 +0200

    added dispatcher and input methods and events

--- js/components/ForumAnswer.react.js
+++ js/components/ForumAnswer.react.js
@@ -1,6 +1,11 @@
 var ForumAnswer = React.createClass({
 	propTypes: {
-		answer: React.PropTypes.object.isRequired
+		answer: React.PropTypes.object.isRequired,
+		onMarkCorrect: React.PropTypes.func.isRequired
+	},
+
+	_markCorrect: function() {
+		this.props.onMarkCorrect(this.props.id);
 	},
 
 	render: function() {
@@ -9,7 +14,11 @@ var ForumAnswer = React.createClass({
 		return (
 				<div className="panel panel-default">
 					<div className="panel-body">
-						{answer.body}	
+						{answer.body}
+						<div className="pull-right">
+							<small><a href="#"	onClick={ this._markCorrect}>
+							Mark as correct</a></small>
+						</div>	
 					</div>
 				</div>
 			);
